753c48b2ccb448da209f424469c6537e202cfe33
Knut Leborg <knut@leborg.co>
2025-06-25 16:31:58 +0200

    cleanup

--- scripts/ExampleTemplateScript.ps1
+++ scripts/ExampleTemplateScript.ps1
@@ -1,3 +1,3 @@
-# BUILD:TEMPLATE(Ps1CmdWrapperHeader)
+# BUILD:TEMPLATE(Ps1CmdWrapperHeader.cmd)
 
 Write-Host "Should be compiled using the provided Ps1CmdWrapperHeader template to be invoked by copying self as temp file to be invoked"
